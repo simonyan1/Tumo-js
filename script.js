ce16b5d613c9b1ebb182a17ca5b332ef02b3d62a
dmartirosyan1 <diana.martirosyan1@tumo.org>
2024-05-19 21:56:08 +0400

    drawEllipse and erase functions

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 createCanvas(800, 600); // Create a canvas of size
-drawLine(200); // Draw a line of length 100
+drawLine(100); // Draw a line of length 100
+drawEllipse(120,60)   // Draw a ellipse of size
+erase()   // Clears the entire canvas
+
