a1d7453493749f72fb3191767643b5f4afff2fd9
yurbeg <yur.beglaryan@gmail.com>
2024-05-20 21:05:51 +0400

    drawTriangle color

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 createCanvas(800, 600); // Create a canvas of size
-drawLine(200); // Draw a line of length 100
+// drawLine(200); // Draw a line of length 100
+
+color(0,255,128)    
+color("#f4d13b")
+color("red")
+color([0,255,128])   
+drawTriangle(150)
