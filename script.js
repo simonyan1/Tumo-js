ad914f8109932ff67c82d23fe933a8935be9a5f8
dmartirosyan1 <diana.martirosyan1@tumo.org>
2024-05-22 23:25:08 +0400

    fix conflict

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-createCanvas(800, 600); // Create a canvas of size
-drawLine(100); // Draw a line of length 100
-drawEllipse(120,60)   // Draw a ellipse of size
-erase()   // Clears the entire canvas
+createCanvas(800, 600); // Create a canvas
+// Example usage
+goTo(200,550);
+drawLine(200);
+right(90);
+drawLine(150);
+left(30);
+drawLine(50);   
+drawEllipse(120,80);
+//erase();
+
 
