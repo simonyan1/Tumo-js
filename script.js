279c29d2faecef632e792b401832a8282d42e298
simonyan1 <varazdat.simonyan@tumo.org>
2024-05-22 16:33:43 +0400

    Fix x,y position

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-createCanvas(800, 600); // Create a canvas of size
+createCanvas(800, 600); // Create a canvas
 // Example usage
+goTo(200,350);
 drawLine(200);
 turnRight(90);
 drawLine(150);
 turnLeft(30);
-drawLine(50);
+drawLine(50);   
 
 
