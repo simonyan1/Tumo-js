24858179dde535b05a59c0f28f4514541161c4af
yurbeg <yur.beglaryan@gmail.com>
2024-05-22 22:06:37 +0400

    bug fixed

--- script.js
+++ script.js
@@ -2,7 +2,4 @@ createCanvas(800, 600); // Create a canvas of size
 // drawLine(200); // Draw a line of length 100
 
 color(0,255,128)    
-color("#f4d13b")
-color("red")
-color([0,255,128])   
-drawTriangle(150)
+drawTriangle(100)
