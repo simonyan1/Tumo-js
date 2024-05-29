75053303e093df5038ce85b173aa46c58aa0e664
AAArmine <aaarmine.aghababyan@gmail.com>
2024-05-29 17:54:00 +0400

    Creating rotate function

--- script.js
+++ script.js
@@ -3,26 +3,10 @@ createCanvas(800, 600); // Create a canvas
 // goTo(200,550);
 // drawLine(200);
 // right(90);
-// drawLine(150);
 // left(30);
-// drawLine(50);   
-// drawEllipse(120,80);
-// // erase();
-// right(90)
-// goTo(200,150)
-// drawPolygon(5,100)
-// showGrid(40)
-// goTo(250,350)
-// drawLine(50);
-// drawPolygon(6,100)
-// drawLine(50);
 
-// drawEllipse(120,80);
-// drawEllipse(120,80);
 rotate(30)
 drawLine(50)
-// drawTriangle(100)
-// drawEllipse(100,100)
 drawPolygon(7,100)
 
 
