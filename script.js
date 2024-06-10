3d8554dffd9519d1b1c03f1a113e889bba4c878a
yurbeglaryan <yur.beglaryan@gmail.com>
2024-06-10 13:54:53 +0400

    fixed without argument

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 createCanvas(800, 600); // Create a canvas of size
+// color("blue")
+// left(45)
+// right(90)
 drawLine(200); // Draw a line of length 100
+
+goTo(100,200)
+// right(50)
+color(255),
+drawTriangle(150)
