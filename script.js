19d816bc8eeb9281558e55b721438215dabaf0b8
dmartirosyan1 <diana.martirosyan1@tumo.org>
2024-06-30 17:32:15 +0400

    Removed ctx.ellipse function from drawEllipse function

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
 createCanvas(800, 600); // Create a canvas
 // // Example usage
-// goTo(200,550);
-// drawLine(200);
-// right(90);
-// left(30);
 
-rotate(30)
-drawLine(50)
-drawPolygon(7,100)
-
-// createCanvas(800, 600); // Create a canvas of size
-// drawLine(200); // Draw a line of length 100
 goTo(700,500)
-// left("Dfdffd")
-// drawLine(50);   
-drawStar(5, 50)
-goTo(200,200)
-drawStar(5, 30)
+fillColor("red") 
+drawEllipse(120,80);
+
+goTo(100, 100)
+fillColor(120,19,149) 
+drawEllipse(120,80);
 
 
 goTo(700,500)
@@ -25,4 +16,6 @@ fillColor("red")
 erase()
 drawLine(120)
 drawEllipse(140, 100)
+drawLine(50)
+
 
