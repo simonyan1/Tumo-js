5928b748a76fe63bce6c60494eca0acdc921e9ab
manyakhachatryan <manya.khachatryan1@gmail.com>
2024-07-01 23:57:15 +0400

    fix bug drawLine

--- script.js
+++ script.js
@@ -1,44 +1,95 @@
-createCanvas(800, 600); // Create a canvas
+createCanvas(600, 600); // Create a canvas
 // // Example usage
 
-goTo(700,500)
-fillColor("red") 
-drawEllipse(120,80);
 
-goTo(100, 100)
-fillColor(120,19,149) 
-drawEllipse(120,80);
+color("black")
+drawLine(50)
+left(30)
+color("yellow")
+drawLine(100)
+left(60)
+color("red")
+drawLine(150)
 
-goTo(200, 200)
-fillColor([120,60,60]) 
-drawEllipse(120,90)
 
-goTo(300,300)
-fillColor("#aa454512")
-drawEllipse(120,120)
 
-goTo(400, 400)
-drawEllipse(100,100)
+// goTo(700,500)
+// fillColor("red") 
+// drawEllipse(120,80);
+// left(30)
+// drawLine(50)
+// // left(30)
+// drawLine(50)
 
-goTo(400,100)
-fillColor("blue")
-drawRectangle(5,50)
+// drawLine(50)
+// goTo(100, 100)
+// fillColor(120,19,149) 
+// drawEllipse(120,80);
 
-goTo(200,100)
-drawRectangle(100,5)
+// // right(80)
+// goTo(200, 200)
+// fillColor([120,60,60]) 
+// drawEllipse(120,90)
 
-goTo(600,200)
-fillColor("yellow")
-drawRectangle(200,200)
+// goTo(300,300)
+// fillColor("#aa454512")
+// drawEllipse(120,120)
 
-goTo(150,150)
-fillColor("brown")
-drawStar(5,100)
+// goTo(400, 400)
+// drawEllipse(100,100)
 
-goTo(180,180)
-drawStar(5,100)
-showGrid(50)
+// goTo(400,100)
+// fillColor("blue")
+// drawRectangle(5,50)
 
-goTo(280,280)
-fillColor("green")
-drawStar(5,100)
+// goTo(200,100)
+// drawRectangle(100,5)
+
+// goTo(600,200)
+// fillColor("yellow")
+// drawRectangle(200,200)
+
+// goTo(150,150)
+// fillColor(220,120,12)
+// drawStar(5,100)
+
+// drawLine(100)
+// right(30)
+// // goTo(180,180)
+// drawStar(5,200)
+// showGrid(50)
+
+// goTo(280,280)
+// fillColor("green")
+// drawStar(5,200)
+
+// goTo(600,524)
+// fillColor("brown")
+// drawTriangle(200)
+
+// goTo(700,224)
+// fillColor("blue")
+// drawTriangle(200)
+
+
+// goTo(800,424)
+// // fillColor("blue")
+// drawTriangle(200)
+
+// goTo(800,600)
+// fillColor("yellow")
+// drawTriangle(200)
+
+
+// goTo(150,300)
+// fillColor("yellow")
+// drawPolygon(6,500)
+
+// goTo(300,400)
+// fillColor("pink")
+// drawPolygon(6,200)
+
+
+// goTo(500,700)
+// // fillColor("pink")
+// drawPolygon(6,800)
